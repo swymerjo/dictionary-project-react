b683a6593a9e4098e27797f88a0ae17d5a309ba0
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-11-29 17:24:03 +0100

    changed footer style

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <Dictionary />
       </main>
       <footer className="mt-3">
-        <strong>Dictionary App</strong> by <a href="https://twitter.com/SammyWymer" className="footer-name text-decoration-none">Sammy Wymer</a> with <a href="https://github.com/swymerjo/dictionary-project-react" className="footer-opensource text-decoration-none">Open-Source Code</a>
+        <strong>Dictionary App</strong> by <a href="https://twitter.com/SammyWymer" className="footer-name text-decoration-none">Sammy Wymer</a> with <a href="https://github.com/swymerjo/dictionary-project-react" className="footer-opensource">Open-Source Code</a>
       </footer>
     </div>
     </div>
