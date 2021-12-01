fbdf0602a5fef5c3e69285d7858c3fe35e818f1a
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-01 11:14:00 +0100

    add Phonetics component which displays text and audio phonetics of searched word

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics.js";
 
 export default function Results(props){
     if(props.results) {
@@ -8,6 +9,13 @@ export default function Results(props){
         <h2>
             {props.results.word}
         </h2>
+        {props.results.phonetics.map(function(phonetic, index) {
+            return(
+                <div key={index}>
+                    <Phonetics phonetics={phonetic} />
+                    </div>
+            );
+        })}
         {props.results.meanings.map(function(meaning, index){
 
         return (
