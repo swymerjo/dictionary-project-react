6d3629568aafbc995735a62682ba54b0953fee86
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-04 11:15:43 +0100

    change audio icon hover color

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -7,7 +7,7 @@ export default function Phonetics(props){
     return (
     <div className="Phonetics">
     <a href={props.phonetics.audio} target="_blank" rel="noreferrer"> 
-    <FontAwesomeIcon icon={faPlayCircle}/>
+    <FontAwesomeIcon icon={faPlayCircle} className="audio-icon"/>
 </a>
 <span className="text">
 {props.phonetics.text}
