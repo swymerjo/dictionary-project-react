d98270703b9ae00fbbea19e078781ccf9f160976
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-04 10:30:48 +0100

    add pexels api to return photos of searched word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,30 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props){
 let [word, setWord] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] =useState(false);
+let [photos, setPhotos] =useState(null);
 
-function handleResponse(response){
+function handleDictionaryResponse(response){
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos);
 }
 
     function search(){
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(apiUrl).then(handleDictionaryResponse);
 
-        axios.get(apiUrl).then(handleResponse);
+        let pexelsApiKey = "563492ad6f91700001000001076b1a8164d54357843eb7bd9699249e";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`;
+        let headers= {"Authorization" : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
     }
 
     function handleWordChange(event){
@@ -46,6 +56,7 @@ function handleResponse(response){
         </div>
         </section>
         <Results results={results}/>
+        <Photos photos={photos} />
         </div>
     );
     } else{
